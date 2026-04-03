8531013eedc794cbf29e5908d7e958115fcd0fb1
RVS_60027060 <rvsrujana.123@gmail.com>
2026-04-03 23:17:17 +0530

    Update api.ts

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,15 +1,20 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
 });
 
-API.interceptors.request.use((req) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    req.headers.Authorization = token;
-  }
-  return req;
-});
+API.interceptors.request.use(
+  (req) => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      req.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return req;
+  },
+  (error) => Promise.reject(error)
+);
 
 export default API;
